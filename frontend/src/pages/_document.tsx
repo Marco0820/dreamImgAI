27b6d792407fb1fecd99cea63c1723fbd79caa7f
Marco0820 <lvyuzhen0820@gmail.com>
2025-09-07 22:24:36 +0800

    update

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document(props: any) {
   return (
     <Html lang={props.locale}>
-      <Head />
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4690050292021329"
+          crossOrigin="anonymous"></script>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
